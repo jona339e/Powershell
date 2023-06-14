8a7a8a64a61540d724015fc94be9400852968552
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2023-06-14 12:10:29 +0200

    Update Static IP.ps1

--- Static IP.ps1
+++ Static IP.ps1
@@ -1,6 +1,6 @@
 $DNS = "10.233.143.2"
 New-NetIPAddress –IPAddress 169.254.122.23 -DefaultGateway 169.254.122.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 
-$InterfaceAlias = (Get-NetIPInterface | Where-Object {$_.InterfaceAlias -like "*Ethernet*"}).InterfaceAlias
 
-Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses $DNS
+Get-DnsClientServerAddress
+Set-DNSClientServerAddress "Ethernet" –ServerAddresses ("169.254.1.157", "")
