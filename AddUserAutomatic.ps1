47c1cd2a6b50328ac240ebb0dc774f74f6591644
jona339e <98744422+jona339e@users.noreply.github.com>
2023-06-14 23:11:15 -0700

    Add files via upload

---
+++ AddUserAutomatic.ps1
@@ -0,0 +1,19 @@
+﻿
+$parentOUPath = Get-Content -Path Users.txt -TotalCount 1
+
+
+$users = Import-Csv -Path Users.txt -Delimiter "," -Header "Firstname", "Lastname", "Username", "Password", "DomainMail", "OUName" | Select-Object -Skip 1
+
+
+foreach ($user in $users) {
+    $firstname = $user.Firstname
+    $lastname = $user.Lastname
+    $username = $user.Username
+    $password = $user.Password
+    $domainMail = $user.DomainMail
+    $ouName = $user.OUName
+    $ouPath = "DC=$ouName,$parentOUPath"
+
+
+    New-ADUser -SamAccountName $username -UserPrincipalName "$username@$domainMail" -GivenName $firstname -Surname $lastname -Name "$firstname $lastname" -Enabled $true -AccountPassword (ConvertTo-SecureString -String $password -AsPlainText -Force) -PassThru -Path $ouPath
+}
