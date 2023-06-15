cc5d70d6fc4c1c75eb187a3e11fada7ef68aa076
jona339e <98744422+jona339e@users.noreply.github.com>
2023-06-15 12:16:59 +0200

    Add files via upload

---
+++ FoldersFromOU.ps1
@@ -0,0 +1,13 @@
+﻿
+$filePath = Get-Content -Path OuList.txt -TotalCount 2 | Select-Object -Skip 1
+
+$mainFolderPath = "C:\$filePath"
+
+$subfolderNames = (Get-Content OuList.txt) | Select-Object -skip 2
+
+New-Item -ItemType Directory -Path $mainFolderPath
+
+foreach ($subfolderName in $subfolderNames) {
+$subfolderPath = Join-Path -Path $mainFolderPath -ChildPath $subfolderName
+New-Item -ItemType Directory -Path $subfolderPath
+}
---
+++ ShareForAfdelinger.ps1
@@ -0,0 +1,21 @@
+﻿# Set the folder path
+$folderPath = "C:\Afdelinger"
+
+# Set the share name
+$shareName = "SharedFolder2"
+
+# Set the share description
+$shareDescription = "Shared folder for authenticated users"
+
+# Set the username for removing "Everyone"
+$usernameToRemove = "Everyone"
+
+# Create the share
+$null = New-SmbShare -Name $shareName -Path $folderPath -Description $shareDescription -FullAccess "Authenticated Users"
+
+# Remove "Everyone" from the share permissions
+$null = Get-SmbShareAccess -Name $shareName | Where-Object { $_.AccountName -eq $usernameToRemove } 
+
+# Display completion message
+Write-Output "Share created and permissions updated successfully!"
+Pause
