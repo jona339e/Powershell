deb34fea3ce793c312af498a326b59057d9dab1b
jona339e <98744422+jona339e@users.noreply.github.com>
2023-06-15 12:14:34 +0200

    Add files via upload

---
+++ DL_G_Grupper.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+
+$importFile = Get-Content OuList.txt | Select-Object -Skip 2
+
+$domainName = Get-Content OuList.txt | Select -first 1
+
+$fullPath = "OU=Grupper,$domainName"
+
+$G_GruppePath = "OU=GlobalGrupper,$fullPath"
+$DL_GruppePath = "OU=DomainLocalGrupper,$fullPath"
+
+
+ForEach($item in $importFile){
+
+$GlobalName = "G_$item"
+$DomainLocalName = "DL_$item"
+
+New-ADGroup -Name $GlobalName -GroupScope Global -GroupCategory Security -Path $G_GruppePath
+
+New-ADGroup -Name $DomainLocalName -GroupScope DomainLocal -GroupCategory Security -Path $DL_GruppePath
+
+
+}
