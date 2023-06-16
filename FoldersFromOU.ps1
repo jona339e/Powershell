01ed584bfcc444122afa51f588085a71f9e154a3
Lucas Kramhöft <110811527+LucasLupin@users.noreply.github.com>
2023-06-16 09:20:25 +0200

    Update FoldersFromOU.ps1

--- FoldersFromOU.ps1
+++ FoldersFromOU.ps1
@@ -1,9 +1,9 @@
 ﻿
-$filePath = Get-Content -Path OuList.txt -TotalCount 2 | Select-Object -Skip 1
+$filePath = Get-Content -Path 05OuList.txt -TotalCount 2 | Select-Object -Skip 1
 
 $mainFolderPath = "C:\$filePath"
 
-$subfolderNames = (Get-Content OuList.txt) | Select-Object -skip 2
+$subfolderNames = (Get-Content 05OuList.txt) | Select-Object -skip 2
 
 New-Item -ItemType Directory -Path $mainFolderPath
 
