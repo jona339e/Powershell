b42074fce353ec4c030f306cea7ed44126fc83fd
jona339e <98744422+jona339e@users.noreply.github.com>
2023-06-16 10:03:32 +0200

    Add files via upload

---
+++ 16ExecReadLeder.ps1
@@ -0,0 +1,17 @@
+$filePath = Get-Content -Path "05OuList.txt" -TotalCount 2 | Select-Object -Skip 1
+$mainFolderPath = "C:\$filePath"
+$subfolderNames = (Get-Content "05OuList.txt") | Select-Object -Skip 2
+
+
+
+Foreach ($subfolderName in $subfolderNames) {
+    $subfolderPath = Join-Path -Path $mainFolderPath -ChildPath $subfolderName
+    
+    $AddPermission = New-Object System.Security.AccessControl.FileSystemAccessRule("DL_Ledelse", "ReadAndExecute", "ContainerInherit, ObjectInherit", "None", "Allow")
+    
+    $acl = Get-Acl -Path $subfolderPath
+    $acl.AddAccessRule($AddPermission)
+
+    Set-Acl -Path $subfolderPath -AclObject $acl
+
+}
