dfb7c81f4a719d4bae83ddfa4848afa4bd9a8241
jona339e <98744422+jona339e@users.noreply.github.com>
2023-06-16 09:12:43 +0200

    Update 06OuAutomatic.ps1

--- 06OuAutomatic.ps1
+++ 06OuAutomatic.ps1
@@ -17,3 +17,39 @@ echo $line
 New-ADOrganizationalUnit -name $line -ProtectedFromAccidentalDeletion $false -Path $folderPath
 
 }
+
+
+﻿#Grupper
+
+$rootDN = "DC=kursus23,DC=com"
+
+
+#Create gruppe in Root
+$parentGroupContainer = "Grupper"
+New-ADOrganizationalUnit -Name $parentGroupContainer -ProtectedFromAccidentalDeletion $false
+
+
+
+#Create GlobalGrupper in afdGrupper
+$ouGloablGruppe = "GlobalGrupper"
+
+# Specify the distinguished name (DN) of the parent container
+$parentContainerDNG = "OU=$parentGroupContainer,$rootDN"
+    
+# Create the OU within the parent container
+#$ouPath = "$parentContainerDN"
+New-ADOrganizationalUnit -Name $ouGloablGruppe -Path $parentContainerDNG -ProtectedFromAccidentalDeletion $false
+
+
+
+
+
+#Create DomainLocalGruppe in afdGrupper
+$ouDomainLocalGruppe = "DomainLocalGrupper"
+
+# Specify the distinguished name (DN) of the parent container
+$parentContainerDNDL = "OU=$parentGroupContainer,$rootDN"
+    
+# Create the OU within the parent container
+#$ouPath = "$parentContainerDN"
+New-ADOrganizationalUnit -Name $ouDomainLocalGruppe -Path $parentContainerDNDL -ProtectedFromAccidentalDeletion $false
