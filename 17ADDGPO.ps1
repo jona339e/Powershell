0acf072b61f06a2fc2c0c2cfdbec8352786b2caa
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2023-06-19 08:14:02 +0200

    Add files via upload

---
+++ 17ADDGPO.ps1
@@ -0,0 +1,12 @@
+﻿# Import the Group Policy module
+Import-Module GroupPolicy
+
+# Create a new GPO
+$gpoName = "DrivePAs"
+New-GPO -Name $gpoName -Comment "GPO for drive mapping" -Domain "kursus101.com" | Out-Null
+
+# Link the GPO to the desired OU or domain
+$ou = "DC=kursus101,DC=com"
+New-GPLink -Name $gpoName -Target $ou
+
+Write-Host "GPO 'DriveP' created successfully."
